d9394ea452962696b509d0cefb18d6a71bf312df
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-05-16 21:56:05 +0100

    Add husky and lint staged
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  "*.{js,jsx,ts,tsx,mjs}": "xo",
+  "*.{ts,tsx}": () => "tsc -p tsconfig.json --noEmit --skipLibCheck",
+};

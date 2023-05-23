82b67eea3a567e53938d1e2c2c1136ccf65268b0
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-05-23 21:29:00 +0100

    Make feeds synchronizer a curried function
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/feeds-synchronizer/app.ts
+++ src/apps/feeds-synchronizer/app.ts
@@ -2,48 +2,49 @@
 import * as _ from "lodash-es";
 import { type Kysely } from "kysely";
 import { type DB } from "kysely-codegen";
-import { type Logger } from "pino";
 import type FeedService from "../../common/services/feed-service.js";
+import type makeLogger from "../../common/logger/mod.js";
 
 type FeedSynchronizerDeps = {
   db: Kysely<DB>;
-  log: Logger;
+  logger: typeof makeLogger;
   feedService: FeedService;
 };
 
-const run = async (
-  { db, log, feedService }: FeedSynchronizerDeps,
-  { signal }: { signal: AbortSignal },
-) => {
-  const feeds = await db.selectFrom("feeds").selectAll().execute();
+const runner = ({ db, logger, feedService }: FeedSynchronizerDeps) => {
+  const log = logger("feed-synchronizer-runner");
 
-  log.info("Synching begin");
+  return async (signal: AbortSignal) => {
+    const feeds = await db.selectFrom("feeds").selectAll().execute();
 
-  for (const feed of feeds) {
-    log.info({ feed }, `Synching feed ${feed.url}`);
+    log.info("Synching begin");
 
-    try {
-      const { faildCount, failedReasons, successCount, totalCount } = await feedService.syncFeed(
-        feed,
-        { signal },
-      );
+    for (const feed of feeds) {
+      log.info({ feed }, `Synching feed ${feed.url}`);
 
-      log.info(
-        { failedReasons, faildCount, successCount, totalCount, feed },
-        `Done processing ${feed.url}, ${totalCount} items, with ${successCount} sucessed and ${faildCount} failed`,
-      );
-    } catch (error) {
-      log.error(error, `Sync failed ${feed.url}`);
-    } finally {
-      log.info({ feed }, `Synching synced ${feed.url}`);
+      try {
+        const { faildCount, failedReasons, successCount, totalCount } = await feedService.syncFeed(
+          feed,
+          { signal },
+        );
+
+        log.info(
+          { failedReasons, faildCount, successCount, totalCount, feed },
+          `Done processing ${feed.url}, ${totalCount} items, with ${successCount} sucessed and ${faildCount} failed`,
+        );
+      } catch (error) {
+        log.error(error, `Sync failed ${feed.url}`);
+      } finally {
+        log.info({ feed }, `Synching synced ${feed.url}`);
+      }
+
+      if (signal.aborted) {
+        break;
+      }
     }
 
-    if (signal.aborted) {
-      break;
-    }
-  }
-
-  log.info(`Synching ended`);
+    log.info(`Synching ended`);
+  };
 };
 
-export default run;
+export default runner;

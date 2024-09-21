59ba8e156aee61afd05ff9792f1662cf5184617d
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-09-21 19:23:45 +0100

    Use @m4rc3l05/pretty-logs
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- .bin/pretty-logs.ts
+++ .bin/pretty-logs.ts
@@ -1,68 +1,7 @@
-#!/usr/bin/env -S deno run -A
+#!/usr/bin/env deno
 
-import type { LevelName } from "@std/log";
-import { TextLineStream } from "@std/streams";
+import { pretty, resolveRuntime, runtime } from "@m4rc3l05/pretty-logs";
 
-const jsonParse = (data: string) => {
-  try {
-    return JSON.parse(data);
-  } catch {
-    //
-  }
-};
-
-const levelColor: Record<LevelName, string> = {
-  "INFO": "color:cyan",
-  CRITICAL: "color:darkred",
-  DEBUG: "color:purple",
-  ERROR: "color:red",
-  WARN: "color:yellow",
-  NOTSET: "color:reset",
-};
-
-// Make sure to wait for stdin to close to close the script
-if (!Deno.stdin.isTerminal()) {
-  Deno.addSignalListener("SIGINT", () => {});
-}
-
-const stdinByLines = Deno.stdin.readable
-  .pipeThrough(new TextDecoderStream())
-  .pipeThrough(new TextLineStream());
-
-for await (const line of stdinByLines) {
-  const jsonParsed = jsonParse(line);
-
-  if (!jsonParsed || typeof jsonParsed !== "object") {
-    console.log(line);
-
-    continue;
-  }
-
-  const { datetime, name, level, message, ...rest } = jsonParsed;
-
-  if (datetime && name && level) {
-    console.log(
-      `%c[${datetime}] %c(${name}) %c${level}: %c${message}%c${
-        Object.keys(rest ?? {}).length > 0
-          ? `\n${
-            Deno.inspect(rest, {
-              colors: true,
-              compact: false,
-              strAbbreviateSize: Number.POSITIVE_INFINITY,
-              depth: 1000,
-              sorted: true,
-              trailingComma: true,
-            })
-          }`
-          : ""
-      }`,
-      "color:lightpink",
-      "color:orange",
-      levelColor[level as LevelName],
-      "color:default",
-      "color:reset",
-    );
-  } else {
-    console.log(jsonParsed);
-  }
+if (import.meta.main) {
+  await pretty(resolveRuntime(runtime()!));
 }
